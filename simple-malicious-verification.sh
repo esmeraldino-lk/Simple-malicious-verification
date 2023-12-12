2cf3062faccc7c7e9d5572b2be0c16d40372ac18
esmeraldino-lk <133903212+esmeraldino-lk@users.noreply.github.com>
2023-12-12 16:03:59 -0300

    Create simple-malicious-verification.sh

---
+++ simple-malicious-verification.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+# By Lucas Esmeraldino
+
+###COLORS
+BLACK='\033[0;30m'        # Black
+RED='\033[0;31m'          # Red
+GREEN='\033[0;32m'        # Green
+YELLOW='\033[0;33m'       # Yellow
+BLUE='\033[0;34m'         # Blue
+PURPLE='\033[0;35m'       # Purple
+CYAN='\033[0;36m'         # Cyan
+WHITE='\033[0;37m'        # White
+
+# Bold
+BBlack='\033[1;30m'       # Black
+BRED='\033[1;31m'         # Red
+BGREEN='\033[1;32m'       # Green
+BYELLOW='\033[1;33m'      # Yellow
+BBLUE='\033[1;34m'        # Blue
+BPURPLE='\033[1;35m'      # Purple
+BCYAN='\033[1;36m'        # Cyan
+BWHITE='\033[1;37m'       # White
+
+#MAIN VARIABLES
+#BashHistory=$(sudo find / -name ".bash_history" 2>/dev/null)
+
+
+
+#FINDING UPONKIT PRIVILLEGE SCALATION
+findSTR=$(sudo find / -name "accountsservice" 2>/dev/null)
+findSTR1=$(sudo find / -name "gnome-control-center" 2>/dev/null)
+
+#BOOLEANS
+accBool=false
+controlBool=false
+
+### FINDING FOR UPONKIT CONDITIONS
+
+#CHECK FOR ACCOUNTS SERVICE
+if [ "${#findSTR}" != 0 ]
+then
+        printf "[!] Accountsservice exist.\n"
+        accBool="true"
+else
+        printf "[*] Accountsservice doesn't exist.\n"
+fi
+
+#CHECK FOR GNOME CONTROL CENTER 
+if [ "${#findSTR1}" != 0 ]
+then
+        printf "[!] Gnome-control-center exist.\n"
+        controlBool="true"
+else
+        printf "[*] Gnome-control-center doesn't exist.\n"
+fi
+
+#UPON KIT CHECK
+if [ "$accBool" != "false" ] && [ "$controlBool" != "false" ];
+then
+        printf "$BRED[!] Can do UponKit.$WHITE"
+else
+        printf "$BGREEN[*] Can't do UponKit.$WHITE"
+fi
+
+#### FINDING TRIES TO UPONKIT
+
+BashHistory=$(sudo find / -name ".bash_history" 2>/dev/null)
+
+printf "\n$YELLOW[*]$WHITE Finding UponKit tries"
+
+for i in $BashHistory
+do
+        printf "$YELLOW"
+        cat $BashHistory | grep -E 'time dbus-send'
+        printf "$WHITE[!]$i $BGREEN CHECKED\n$WHITE"
+done
+
+#### FINDING COMMON PRIVILLEGE SCALATION
+
+### IN BASH HISTORY
+
+BashHistory=$(sudo find / -name ".bash_history" 2>/dev/null)
+
+printf "$YELLOW[*]$WHITE Search for common privillege scalation commands in $YELLOWbash history.\n$WHITE"
+for i in $BashHistory
+do
+        printf "$YELLOW"
+        cat $BashHistory | grep -E 'pwned|crackmapexec|nmap|fscan|rootkit|heroin|trojan|nc -lvnp|nc -lnvp|worm|python|ruby|sudo su -l|whoami|wget|whoami|kit|upon|uponkit|dbus-send|sudo -u#-1 /bin/bash|find / -perm -u=s -type f 2>/dev/null|WafW00f|ssh|hydra'
+        printf "$WHITE[!] $i $BGREEN CHECKED\n$WHITE"
+done
+
+
+### IN FILES
+
+printf "$YELLOW[*]$WHITE Search for suspect files.\n$YELLOW"
+
+find / -name "linpeas" -o -name "linenum" -o -name "winpeas" 2>/dev/null
+find / -name "uponkit" -o -name "upon" -o -name "rootkit" 2>/dev/null
+find / -name "pwned" -o -name "hack" -o -name "hacked" 2>/dev/null
+
+printf "$BGREEN[*] Done."
