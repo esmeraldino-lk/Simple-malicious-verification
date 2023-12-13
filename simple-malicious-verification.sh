d613b7b1da7e45c3461f1fd80f39643ac304a738
esmeraldino-lk <133903212+esmeraldino-lk@users.noreply.github.com>
2023-12-13 11:52:42 -0300

    Update simple-malicious-verification.sh

--- simple-malicious-verification.sh
+++ simple-malicious-verification.sh
@@ -97,7 +97,7 @@ printf "$YELLOW[*]$WHITE Search for suspect files.\n$YELLOW"
 sudo find / -name "linpeas" -o -name "linenum" -o -name "winpeas"
 sudo find / -name "uponkit" -o -name "upon" -o -name "rootkit"
 sudo find / -name "pwned" -o -name "hacker" -o -name "hacked"
-sudo find / -name "enum" -o -name "payload" -o -name "trojan" -o -name "exploit"
+sudo find / -name "enum" -o -name "payload" -o -name "trojan" -o -name "exploit" -o -name "shell" -o -name "liferay"
 
 
 printf "$YELLOW[*]$WHITE Verifying for listening ports.\n$YELLOW"
